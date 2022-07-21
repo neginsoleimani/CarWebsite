b134971f2a24a40da58b12c8d05aa60ff35cd135
Negin Soleiamni <negin.kdk1@gmail.com>
2022-07-21 16:15:33 +0430

    change header color by js

--- asset/script/script.js
+++ asset/script/script.js
@@ -23,3 +23,15 @@ function linkAction() {
     navMenu.classList.remove("show-menu")
 }
 navLink.forEach(n => n.addEventListener("click",linkAction))
+
+// Change Background Header
+function scrollHeader(){
+    const header=document.getElementById("header")
+    if(this.scrollY>=50){
+        header.classList.add('scroll-header');
+    }else{
+        header.classList.remove('scroll-header')
+    }
+}
+
+window.addEventListener("scroll",scrollHeader)
