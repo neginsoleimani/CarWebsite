7e6c21223ecc35008162c5b02580cc0207f84235
Negin Soleiamni <negin.kdk1@gmail.com>
2022-07-23 13:03:44 +0430

    add scroll up button and scroll active link

--- asset/script/script.js
+++ asset/script/script.js
@@ -1,4 +1,5 @@
 // Show Menu
+
 const navMenu = document.querySelector("#nav-menu")
 const navToggle = document.querySelector("#nav-toggle")
 const navClose = document.querySelector("#nav-close")
@@ -16,6 +17,7 @@ if (navClose) {
 }
 
 // Remove Mobile Menu
+
 const navLink = document.querySelectorAll(".nav-link")
 
 function linkAction() {
@@ -25,6 +27,7 @@ function linkAction() {
 navLink.forEach(n => n.addEventListener("click", linkAction))
 
 // Change Background Header
+
 function scrollHeader() {
     const header = document.getElementById("header")
     if (this.scrollY >= 50) {
@@ -36,6 +39,7 @@ function scrollHeader() {
 window.addEventListener("scroll", scrollHeader)
 
 //Swiper
+
 let swiper = new Swiper(".popular-container", {
     loop: true,
     spaceBetween: 24,
@@ -56,6 +60,7 @@ let swiper = new Swiper(".popular-container", {
 });
 
 //Mixitup
+
 let mixerFeatured = mixitup('.featured-content', {
     selectors: {
         target: '.featured-card'
@@ -66,11 +71,51 @@ let mixerFeatured = mixitup('.featured-content', {
 });
 
 // Link Featured Active
+
 const featuredLink = document.querySelectorAll('.featured-item')
 
-function activeFeatured(){
-    featuredLink.forEach(l=>l.classList.remove('active-featured'))
+function activeFeatured() {
+    featuredLink.forEach(l => l.classList.remove('active-featured'))
     this.classList.add('active-featured')
 }
-featuredLink.forEach(l=>l.addEventListener("click",activeFeatured))
+featuredLink.forEach(l => l.addEventListener("click", activeFeatured))
+
+
+//Scroll Up
+
+function scrollUp() {
+    const scrollUp = document.getElementById("scroll-up")
+    if (this.scrollY >= 350) {
+        scrollUp.classList.add("show-scroll")
+    } else {
+        scrollUp.classList.remove("show-scroll")
+    }
+}
+
+window.addEventListener("scroll", scrollUp)
+
+
+//Scroll Section Active Link
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+            const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute("id")
+
+        if (scrollY >= sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add("active-link")
+        } else {
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove("active-link")
+        }
+
+    })
+}
+
+window.addEventListener("scroll", scrollActive)
+
 
