007717ca41b1373628b9204b8a2e4b7d090735cf
Negin Soleiamni <negin.kdk1@gmail.com>
2022-07-23 17:46:59 +0430

    add animation by scrollrevealjs

--- asset/script/script.js
+++ asset/script/script.js
@@ -125,7 +125,35 @@ function scrollActive() {
 
     })
 }
-
 window.addEventListener("scroll", scrollActive)
+
+
+// Scroll Reveal 
+
+const sr = ScrollReveal({
+    origin:'top',
+    distance:'60px',
+    duration:2500,
+    delay:400,
+})
+
+sr.reveal(`.home-title , .popular-container , .features-img , .featured-filters , .footer-copy`)
+sr.reveal(`.home-subtitle`,{delay:500})
+sr.reveal(`.home-elec`,{delay:600})
+sr.reveal(`.home-img`,{delay:800})
+sr.reveal(`.home-car-data`,{delay:900,interval:100,origin:'bottom'})
+sr.reveal(`.home-button`,{delay:1000,origin:'bottom'})
+sr.reveal(`.about-group , .offer-data`,{origin:'left'})
+sr.reveal(`.about-data , .offer-img`,{origin:'right'})
+sr.reveal(`.features-map`,{delay:600,origin:'bottom'})
+sr.reveal(`.features-card`,{interval:300})
+sr.reveal(`.featured-card , .logos-content , .footer-content`,{interval:100})
+
+
+
+
+
+
+
 
 
